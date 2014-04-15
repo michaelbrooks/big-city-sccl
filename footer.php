fb781f1a19b156437f03810655a59b4d72c51eed
SCCL <sccl@uw.edu>
2014-04-15 15:30:21 -0700

    add period in footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div> <!--#sticky-wrap-->
 <div id="footer">
     <div class="footer-container">
-        <p class="copyright">&copy; <?php echo date('Y');?> <?php bloginfo( 'name' ); ?> All Rights Reserved.
+        <p class="copyright">&copy; <?php echo date('Y');?> <?php bloginfo( 'name' ); ?>. All Rights Reserved.
 	<a href="http://depts.washington.edu/sccl/members/">Members</a>
 	</p>
         <div id="base-widgets">
